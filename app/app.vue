438f7e2be106c0b1959e3ea4e1b599c8985e92bc
Fher03 <feransic@gmail.com>
2026-02-28 16:34:32 -0600

    feat: Se implementa la seccion Agradecimientos

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="bg-[url(/bg.jpg)]">
-    <SectionPrincipalComponent />
+  <main class="bg-[url(/bg.jpg)] flex flex-col items-center">
+    <SectionPrincipal />
+    <SectionGratitude />
   </main>
 </template>
