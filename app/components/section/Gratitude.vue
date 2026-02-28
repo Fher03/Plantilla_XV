a6e2ac4089c93a5dd05cc16e91d0ee5744060439
Fher03 <feransic@gmail.com>
2026-02-28 16:34:32 -0600

    feat: Se implementa la seccion Agradecimientos

---
+++ app/components/section/Gratitude.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col gap-20 h-160 w-screen xl:w-100 py-10 px-5">
+    <h1 class="font-pinyon-script text-5xl text-center">Agradecimientos</h1>
+    <!-- Card -->
+    <div
+      class="relative w-6/7 h-100 bg-white border-4 border-primary-dark p-5 text-3xl text-center flex flex-col gap-5 font-great-vibes"
+    >
+      <p>Con la bendicion de:</p>
+      <!-- Nombres completos de los padres -->
+      <p>Luz</p>
+      <p>Neto</p>
+      <NuxtImg
+        class="absolute -bottom-20 -right-15 w-40 h-40"
+        src="/flowers/flower_1.png"
+      />
+    </div>
+  </div>
+</template>
