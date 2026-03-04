fe1be7715512b23ec2ec66f3f0eb51f11e4d89e3
Fher03 <feransic@gmail.com>
2026-03-03 22:17:48 -0600

    feat: Se implementa la seccion sube las fotos

---
+++ app/components/section/UploadPhotos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="relative bg-[url(/photos/upload_photos.jpg)] bg-center bg-cover bg-no-repeat w-full h-screen md:bg-contain"
+  >
+    <div class="absolute z-1 bg-black/50 bg-center w-full h-full" />
+    <div
+      class="relative z-2 h-full flex flex-col items-center justify-between py-10"
+    >
+      <h2
+        class="text-center font-great-vibes text-white w-sm text-4xl leading-14"
+      >
+        Sube tus fotos y videos de mis XV
+      </h2>
+      <div>
+        <p class="text-white text-2xl">Aqui Sube tus fotos!</p>
+      </div>
+      <!-- <input type="file" /> -->
+
+      <a
+        href="https://www.instagram.com/explore/tags/misXvZoe/"
+        class="text-white flex items-center gap-2 rounded-2xl bg-primary-dark py-2 px-8 font-sans"
+        ><Icon name="mdi:instagram" class="text-2xl" />#MisXVZoe</a
+      >
+    </div>
+  </div>
+</template>
