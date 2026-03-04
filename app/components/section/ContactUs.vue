3fbeaacf1b359141ca183dbdceb559969e012e9b
Fher03 <feransic@gmail.com>
2026-03-03 22:46:24 -0600

    feat: Se termina de implementar la seccion final

---
+++ app/components/section/ContactUs.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="h-fit w-screen">
+    <div
+      class="flex flex-col py-10 items-center justify-center gap-5 bg-white h-full"
+    >
+      <h3 class="text-lg">WebVite - Invitaciones digitales</h3>
+      <p class="text-md">Whatsapp: +52 828 120 3532</p>
+      <a
+        class="bg-green-300 text-white rounded-sm px-4 py-2 flex items-center gap-2 justify-center"
+        href="https://wa.me/8281203532?text=Hola, me gustaría saber más sobre las invitaciones"
+      >
+        <Icon name="simple-icons:whatsapp" class="text-xl" />
+        <span class="text-green-900 font-semibold"> Enviar Whatsapp </span>
+      </a>
+    </div>
+  </section>
+</template>
