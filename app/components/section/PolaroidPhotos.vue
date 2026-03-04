78d8d4ff454bbec86ca102230eb64617f813eac8
Fher03 <feransic@gmail.com>
2026-03-03 22:28:01 -0600

    feat: Se implementa la seccion fotos polaroid

---
+++ app/components/section/PolaroidPhotos.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex flex-col items-center gap-15 h-fit w-screen xl:w-100 py-10 px-5"
+  >
+    <NuxtImg
+      v-for="(_, index) in new Array(8)"
+      :key="index"
+      :src="`/photos/polaroid_${index + 1}.png`"
+    />
+  </div>
+</template>
