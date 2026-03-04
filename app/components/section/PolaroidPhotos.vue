a7da718409c4b9417b536ef9852511ca300575be
Fher03 <feransic@gmail.com>
2026-03-03 22:30:23 -0600

    feat: Se elimina gap entre imagenes polaroid

--- app/components/section/PolaroidPhotos.vue
+++ app/components/section/PolaroidPhotos.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex flex-col items-center gap-15 h-fit w-screen xl:w-100 py-10 px-5"
-  >
+  <div class="flex flex-col items-center h-fit w-screen xl:w-100 py-10 px-5">
     <NuxtImg
       v-for="(_, index) in new Array(8)"
       :key="index"
