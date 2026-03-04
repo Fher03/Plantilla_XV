2d9699088a2703b5aa171f36d6bd6bc57702bb11
Fher03 <feransic@gmail.com>
2026-03-03 22:02:45 -0600

    feat: Se implementa la seccion codigo de vestimenta y mesa de regalos

---
+++ app/components/section/DressCode.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="flex flex-col items-center gap-15 h-fit w-screen xl:w-100 py-10 px-5"
+  >
+    <!-- Title -->
+    <div
+      class="text-center flex flex-col gap-2 py-4 px-10 font-great-vibes bg-white text-primary-dark border-4 border-primary-dark shadow-2xl"
+    >
+      <h2 class="text-4xl">Codigo de Vestimenta</h2>
+      <h3 class="text-3xl">Formal</h3>
+    </div>
+    <div class="flex justify-center gap-10">
+      <img src="/icons/smokin_icon.svg" class="w-40" />
+      <img src="/icons/dress_icon.svg" class="w-25" />
+    </div>
+    <div class="flex items-center justify-center max-w-xs">
+      <div
+        class="text-center flex flex-col gap-2 p-4 font-serif bg-white text-primary-dark border-4 border-primary-dark shadow-2xl"
+      >
+        <h3 class="text-base text-center">
+          Color Rosa reservado para la quinceañera
+        </h3>
+      </div>
+    </div>
+    <div
+      class="text-center flex flex-col gap-2 py-4 px-10 font-great-vibes bg-white text-primary-dark border-4 border-primary-dark shadow-2xl"
+    >
+      <h2 class="text-4xl">Mesa de regalos</h2>
+      <h3 class="text-3xl">Cualquier regalo es bienvenido</h3>
+    </div>
+    <div class="flex justify-center gap-10">
+      <img src="/icons/mail_icon.svg" class="w-25" />
+      <img src="/icons/gift_icon.svg" class="w-25" />
+    </div>
+  </div>
+</template>
